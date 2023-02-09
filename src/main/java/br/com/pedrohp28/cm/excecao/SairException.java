693b8cd2e17c49e0f7ace3023dad66a666650a47
Pedro Henrique Pereira <pedrohp36@gmail.com>
2023-02-09 19:20:59 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/pedrohp28/cm/excecao/SairException.java
@@ -0,0 +1,6 @@
+package br.com.pedrohp28.cm.excecao;
+
+public class SairException extends RuntimeException{
+
+    private static final long serialVersionUID = 1;
+}
---
+++ src/main/java/br/com/pedrohp28/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,82 @@
+package br.com.pedrohp28.cm.visao;
+
+import br.com.pedrohp28.cm.excecao.ExplosaoException;
+import br.com.pedrohp28.cm.excecao.SairException;
+import br.com.pedrohp28.cm.modelo.Tabuleiro;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class TabuleiroConsole {
+
+    private Tabuleiro tabuleiro;
+    private Scanner scan = new Scanner(System.in);
+
+    public TabuleiroConsole(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+
+        executarJogo();
+    }
+
+    private void executarJogo() {
+        try{
+            boolean continuar = true;
+            while (continuar) {
+                cicloDoJogo();
+
+                System.out.println("Outra partida? (S/n) ");
+                String resposta = scan.nextLine();
+
+                if ("n".equalsIgnoreCase(resposta)) {
+                    continuar = false;
+                } else {
+                    tabuleiro.reiniciar();
+                }
+            }
+        } catch (SairException e) {
+            System.out.println("Tchau!!!");
+        } finally {
+            scan.close();
+        }
+    }
+
+    private void cicloDoJogo() {
+        try {
+
+            while (!tabuleiro.objetivoAlcancado()) {
+                System.out.println(tabuleiro);
+
+                String digitado = capturarValorDigitado("Digite (x, y): ");
+
+                Iterator<Integer> xy = Arrays.stream(digitado.split(","))
+                        .map(e -> Integer.parseInt(e.trim())).iterator();
+
+                digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)Marcar: ");
+
+                if ("1".equalsIgnoreCase(digitado)) {
+                    tabuleiro.abrir(xy.next(), xy.next());
+                } else if("2".equalsIgnoreCase(digitado)) {
+                    tabuleiro.alternarMarcacao(xy.next(), xy.next());
+                }
+            }
+
+            System.out.println(tabuleiro);
+            System.out.println("Você ganhou!!!");
+        } catch(ExplosaoException e) {
+            System.out.println(tabuleiro);
+            System.out.println("Você perdeu!");
+        }
+    }
+
+    private String capturarValorDigitado(String texto){
+        System.out.print(texto);
+        String digitado = scan.nextLine();
+
+        if ("sair".equalsIgnoreCase(digitado)) {
+            throw new SairException();
+        }
+
+        return digitado;
+    }
+}
